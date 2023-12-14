bee17933c0552c3ba0fa6a0319b2acb6b1417ab9
Alexandr <alexandr.florinskiy@vgs.io>
2023-12-14 12:55:58 +0200

    add reset + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -14,7 +12,10 @@ function App() {
     setCount((count) => parseInt(count) + 1)
   }
   function decreaseCount() {
-    const a = setCount((count) => parseInt(count) - 1)
+    setCount((count) => parseInt(count) - 1)
+  }
+  function resetCount() {
+    setCount(0)
   }
   useEffect(() => {
     localStorage.setItem('score', count)
@@ -25,31 +26,12 @@ function App() {
   
   return (
     <div className='wr'>
-      <div className="view">{count}</div>
-      <div className="dec" onClick={decreaseCount}>-</div>
-      <div className="inc" onClick={increaseCount}>+</div>
-
-      
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="view">
+        {count}
+        <button className='button reset' onClick={resetCount}>X</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <div className="button dec" onClick={decreaseCount}>-</div>
+      <div className="button inc" onClick={increaseCount}>+</div>
     </div>
   )
 }
